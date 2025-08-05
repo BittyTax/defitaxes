d502864814a14347875d67dad46ea2bf4511705c
Scott Green <47545109+nanonanouk@users.noreply.github.com>
2025-08-05 13:00:15 +0100

    Telegram alerts: disable if env variables not configured

--- telegram_alerts.py
+++ telegram_alerts.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import os
 import time
+from typing import List
 
 from telethon import TelegramClient
 
@@ -10,26 +11,37 @@ class TelegramAlertsHandler(logging.Handler):
     MAX_ALERTS_PER_MINUTE = 5
     ALERT_COOLDOWN_SECONDS = 60
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(logging.ERROR)
-        self.alert_timestamps = []
-        self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(self.loop)
+        self.initialised = False
+        self.alert_timestamps: List[float] = []
 
-        self.client = TelegramClient(
-            "bot",
-            os.environ.get("DEFITAXES_TELEGRAM_API_ID"),
-            os.environ.get("DEFITAXES_TELEGRAM_API_HASH"),
-        )
-        self.client.start(bot_token=os.environ.get("DEFITAXES_TELEGRAM_BOT_TOKEN"))
+        try:
+            api_id = os.environ.get("DEFITAXES_TELEGRAM_API_ID")
+            api_hash = os.environ.get("DEFITAXES_TELEGRAM_API_HASH")
+            bot_token = os.environ.get("DEFITAXES_TELEGRAM_BOT_TOKEN")
+            username = os.environ.get("DEFITAXES_TELEGRAM_ALERT_USER")
 
-        self.loop.run_until_complete(
-            self._get_entity(os.environ.get("DEFITAXES_TELEGRAM_ALERT_USER"))
-        )
+            if not api_id or not api_hash or not bot_token or not username:
+                return
 
-    def emit(self, record):
+            self.loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(self.loop)
+
+            self.client = TelegramClient("bot", api_id, api_hash)
+            self.client.start(bot_token=bot_token)
+
+            self.loop.run_until_complete(self._get_entity(username))
+            self.initialised = True
+
+        except (ValueError, asyncio.TimeoutError, ConnectionError):
+            self.initialised = False
+
+    def emit(self, record: logging.LogRecord) -> None:
+        if not self.initialised:
+            return
+
         current_time = time.time()
-
         self.alert_timestamps = [
             ts for ts in self.alert_timestamps if current_time - ts < self.ALERT_COOLDOWN_SECONDS
         ]
@@ -41,14 +53,12 @@ class TelegramAlertsHandler(logging.Handler):
         message = self.format(record)
         self.loop.run_until_complete(self._send_message(message))
 
-    async def _get_entity(self, username):
+    async def _get_entity(self, username: str) -> None:
         if not self.client.is_connected():
             return
-
         self.peer = await self.client.get_entity(username)
 
-    async def _send_message(self, message):
+    async def _send_message(self, message: str) -> None:
         if not self.client.is_connected():
             return
-
         await self.client.send_message(self.peer, message)
