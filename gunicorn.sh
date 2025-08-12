3e7815faffa69438aeb48edb561106f9179cedd2
Scott Green <47545109+nanonanouk@users.noreply.github.com>
2025-08-12 19:44:34 +0100

    Gunicorn: added --preload option
    Added --preload so that the worker processes share the same memory. This prevents issues with the Telegram alerts client.

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 source ./.venv/bin/activate
-gunicorn -w 4 -b unix:/tmp/gunicorn.sock --access-logfile - --error-logfile - wsgi:application
+gunicorn -w 4 -b unix:/tmp/gunicorn.sock --access-logfile - --error-logfile - --preload wsgi:application
