2e09a11a3f01ed1324113e3b2cb695831e51eea5
Scott Green <47545109+nanonanouk@users.noreply.github.com>
2025-10-24 20:09:53 +0100

    Added OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
    This is required to keep APScheduler happy on macOS when it forks processes.

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 source ./.venv/bin/activate
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 gunicorn -w 4 -b unix:/tmp/gunicorn.sock --access-logfile - --error-logfile - --preload wsgi:application
