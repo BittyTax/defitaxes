ee1c04b8fe57e3b9a1c34b9e49582a77c8c11a5a
Scott Green <47545109+nanonanouk@users.noreply.github.com>
2026-01-16 17:59:49 +0000

    Gunicorn: store access and error logs
    The gunicorn access and errors logs are kept in instance/logs/gunicorn.

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,4 +2,4 @@
 
 source ./.venv/bin/activate
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-gunicorn -t 300 -w 4 -b unix:/tmp/gunicorn.sock --access-logfile - --error-logfile - --preload wsgi:application
+gunicorn -t 300 -w 4 -b unix:/tmp/gunicorn.sock --access-logfile instance/logs/gunicorn/access.log --error-logfile instance/logs/gunicorn/error.log --preload wsgi:application
